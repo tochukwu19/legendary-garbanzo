ffa8b5482f61fdba91ef5820c06271f91f694866
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-04-06 17:05:02 +0000

    Implemented a loader

--- dist/script/app.js
+++ dist/script/app.js
@@ -29,6 +29,24 @@ function panelList2Fn() {
     panelList3.classList.remove("active1")
 }
 
+const preloader = document.querySelector('.preloader');
+const fadeEffect = setInterval(() => {
+    // if we don't set opacity 1 in CSS, then
+    // it will be equaled to "" -- that's why
+    // we check it, and if so, set opacity to 1
+    if (!preloader.style.opacity) {
+        preloader.style.opacity = 1;
+    }
+    if (preloader.style.opacity > 0) {
+        preloader.style.opacity -= 0.08;
+    } else {
+        clearInterval(fadeEffect);
+    }
+}, 600);
+
+
+window.addEventListener('load', fadeEffect);
+
 panelList3.addEventListener("click", panelList3Fn)
 
 function panelList3Fn() {
